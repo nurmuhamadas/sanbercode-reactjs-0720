2b3e1b778e49cec32221bb0169ee240f3a54c22f
Nur Muhamad Ash Shidiqi <nurmuhamad.a.13@gmail.com>
2020-07-24 03:40:40 +0700

    Pekan 1 - Tugas 5, update

--- Pekan 1/Tugas-Hari-Ke-4/tugas4.js
+++ Pekan 1/Tugas-Hari-Ke-4/tugas4.js
@@ -52,4 +52,6 @@ console.log('\n\n\nsoal 5\n');
 
 var daftarBuah = ["2. Apel", "5. Jeruk", "3. Anggur", "4. Semangka", "1. Mangga"];
 
-console.log(daftarBuah.sort());
+daftarBuah.sort().forEach(buah => {
+    console.log(buah);
+})
